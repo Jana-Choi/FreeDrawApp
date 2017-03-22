c240d8bc4ddaff94c163d785c16b1a300745563b
jana-choi <lovej0918@gmail.com>
2017-03-22 10:55:45 +0900

    project space

---
+++ FreeDrawApp/app/src/main/java/org/graphics/jana/freedrawapp/MainActivity.java
@@ -0,0 +1,13 @@
+package org.graphics.jana.freedrawapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
